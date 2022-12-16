6f08128f41ce050d4017fed1a5a85d43841f9509
Alexandre Hauffe <alexandre.hauffe@Alexandres-MacBook-Pro.local>
2022-12-15 22:09:25 -0300

    day 3 part 2

--- advantofcode/day_3/day_3.py
+++ advantofcode/day_3/day_3.py
@@ -17,5 +17,19 @@ print(int(gamma, base=2) * int(epsilon, base=2))
 
 
 
+gamma, epsilon = '', ''
+
+for column in range(len(values[0])):
+    column_list = [line[column] for line in values]
+    if column_list.count('1') >= column_list.count('0') :
+        gamma += '1'
+        epsilon += '0'
+    else:
+        gamma += '0'
+        epsilon += '1'
+print(int(gamma, base=2)* int(epsilon, base=2)) 
+
+
+
 
 
