47776179b846f4e2b0d1e11698ac41c29a45884f
Alexandre <alex_hauffe@hotmail.com>
2023-03-12 15:32:49 -0300

    Day 10 part 1

---
+++ adventofcode/day_10/day_10.py
@@ -0,0 +1,29 @@
+
+open_close_chars = {"{":"}", "(":")", "<":">", "[":"]"}
+points = {")":3, "]":57, "}":1197, ">":25137}
+result = 0
+
+with open('adventofcode/day_10/input.txt', 'r') as f:
+    lines = [line.strip() for line in f.readlines()]
+
+
+def verify_characteres(line):
+    verifying_arr = []
+    for character in line:
+        if character in open_close_chars.keys():
+            verifying_arr.extend(character)
+        else:
+            if open_close_chars.get(verifying_arr[-1]) == character:
+                verifying_arr.pop()
+            else:
+                print(f'expected {open_close_chars.get(verifying_arr[-1])}, but found ')
+                return character
+    return True
+
+
+for line in lines:
+    character = verify_characteres(line)
+    if character != True:
+        print(character)
+        result+= points.get(character)
+        print(result)
